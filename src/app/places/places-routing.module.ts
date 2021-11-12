88ec3f890fc96b27f96f83ac42996bf93ab8fce5
rohan <deepakrohan@hotmail.com>
2021-11-12 10:49:22 -0500

    added the nav from discover to detail

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -5,18 +5,57 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+          },
+          {
+            path: 'new',
+            loadChildren: () => import('./offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule)
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () => import('./offers/edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./discover/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
     path: '',
-    component: PlacesPage
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full'
   }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Place } from './places.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  private places: Place[] = [
+    new Place('p1', 
+    'Manhattan',
+    'In Heart of NYC',
+    'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    149.99),
+    new Place('p2',
+    'Brooklyn',
+    'In Brooklyn',
+    'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    100.99),
+    new Place('p3',
+    'Queens',
+    'In Queens',
+    'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    99.99),
+    new Place('p4',
+    'Bronx',
+    'In Bronx',
+    'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    79.99),
+  ];
+
+  constructor() { }
+
+  getPlaces() {
+    return [...this.places];
+  };
+}
