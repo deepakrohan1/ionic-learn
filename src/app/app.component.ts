820dbc056808f32bc4ff2b12749051027efc3f43
rohan <deepakrohan@hotmail.com>
2021-11-15 16:58:39 -0500

    added auth guard for loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {}
 
   onLogout(): void {
-    
+    this.authService.logout();
+    this.router.navigate(['/auth']);
+
   }
+
+  
 }
