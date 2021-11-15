bc141e94d93ce8280a93bb7abce7cbb9f38c6904
rohan <deepakrohan@hotmail.com>
2021-11-15 14:53:28 -0500

    adding ion-menu across components

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Offer } from '../offers.model';
+import { OffersService } from '../offers.service';
 
 @Component({
   selector: 'app-edit-offer',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOfferPage implements OnInit {
 
-  constructor() { }
+  offer: Offer;
+  
+  constructor(private route: ActivatedRoute, 
+    private offsetService: OffersService) { }
 
   ngOnInit() {
+
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('offerId')) {
+        return;
+      }
+      const offerId = paramMap.get('offerId');
+      this.offer = this.offsetService.getOffer(offerId);
+    });
+
   }
 
 }
