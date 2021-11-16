dfad67fad1a9f1858050c23fa6701c17cc9f9aaa
rohan <deepakrohan@hotmail.com>
2021-11-16 06:46:55 -0500

    some fun stuff with lists

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonItemSliding } from '@ionic/angular';
 import { Offer } from './offers.model';
 import { OffersService } from './offers.service';
 
@@ -11,11 +13,18 @@ export class OffersPage implements OnInit {
 
   offers: Offer[] = [];
 
-  constructor(private offerService: OffersService) { }
+  constructor(private offerService: OffersService, private router: Router) { }
 
 
   ngOnInit() {
     this.offers = this.offerService.getOffers();
   }
+
+  onEdit(offerId: string, slidintgItem: IonItemSliding) {
+    slidintgItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+    console.log('Editing item', offerId);
+  }
+
 
 }
